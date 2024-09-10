0972ac7edde2fb74ea943091b99bc9261b65e34f
GhulamMohiuddin-dev <ghulammohiuddindev@gmail.com>
2024-09-10 19:28:40 +0500

    component name corrected for user role

--- src/config/permissions.ts
+++ src/config/permissions.ts
@@ -2,6 +2,6 @@ export type Role = 'admin' | 'user' | 'guest';
 
 export const permissionsConfig: Record<Role, string[]> = {
   admin: ['Sidebar', 'Header', 'CustomChart', 'CustomTable'],
-  user: ['Sidebar', 'Chart'],
+  user: ['Sidebar', 'CustomChart'],
   guest: ['Sidebar'],
 };
